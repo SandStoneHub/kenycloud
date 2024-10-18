df73b9d1c41376c7ae3ac76d771f00b28749936d
Kenyka <maratkanovartem89@mail.ru>
2024-10-18 14:06:13 +0300

    fix 403 page show bug

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@
 import { useUser } from '@clerk/clerk-react'
 import Page403 from "../errors/403";
 import { SideNav } from "./side-nav"
+import { useEffect, useState } from 'react';
 
 export default function DashboardLayout({
   children,
@@ -11,10 +12,23 @@ export default function DashboardLayout({
 }>) {
 
   const { isSignedIn } = useUser()
+  const [showPage403, setShowPage403] = useState(false)
+
+  useEffect(() => {
+    if (process.env.SERVER_STATE === "True" || !isSignedIn) {
+      const timer = setTimeout(() => {
+        setShowPage403(true)
+      }, 1000)
+
+      return () => clearTimeout(timer)
+    } else {
+      setShowPage403(false)
+    }
+  }, [isSignedIn])
 
   return (
       <>
-        {(process.env.SERVER_STATE === "True" || !isSignedIn) ? (
+        {showPage403 ? (
           <Page403/>
         ) : (
           <main className="container mx-auto pt-12">
